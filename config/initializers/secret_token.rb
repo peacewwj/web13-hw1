e72cedf820ddaad2d5431353c2f7f550cc506388
Santisuk <peacesantisuk@gmail.com>
2013-08-28 14:29:19 +0700

    Initializing repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBook::Application.config.secret_token = '59509d6bf928b2cd2d9baf7ed0da8ecf8445d60e9a8412d50c198905848c1bf52d766f0ae911bb5afa07414f33493d836c90436b8ad1d5a36d2abdc1a76aa016'
